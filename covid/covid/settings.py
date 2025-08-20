6c6f64b69888c6a19280a478e20f58ad0b15fbfb
Saanviya Sunil <saanviya19@gmail.com>
2025-08-20 21:02:45 +0400

    feat: changed to postgres db

--- covid/covid/settings.py
+++ covid/covid/settings.py
@@ -12,8 +12,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
-ALLOWED_HOSTS = ["*"]
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+}
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -79,21 +82,9 @@ WSGI_APPLICATION = 'covid.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'covidDB',
-        'USER': 'root',  
-        'PASSWORD': 'root1234',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'sql_mode': 'traditional',
-        }
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
