8f6d08e67b2faa41a141241c11c8a6a79722cf9e
Saanviya Sunil <saanviya19@gmail.com>
2025-08-20 20:32:46 +0400

    fix: removed Procfile, hosting in render

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn covid.covid.wsgi
--- covid/covid/settings.py
+++ covid/covid/settings.py
@@ -13,11 +13,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+ALLOWED_HOSTS = ["*"]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 
 # Quick-start development settings - unsuitable for production
